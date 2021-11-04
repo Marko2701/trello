6890a4bf23dbbc5d66dbe6302821a0474b8d96be
Marko2701 <marko.mladenovic.bg@gmail.com>
2021-11-04 02:24:02 +0100

    rework

--- main.js
+++ main.js
@@ -1,127 +1,154 @@
-const lists = document.querySelectorAll('.list');
-const button = document.querySelector('.button');
+const lists = document.querySelectorAll(".list");
+const button = document.querySelector(".button");
 
+let boardIdNo = 4;
 
-function addTask() {
-    const btn = document.querySelector('.add__btn');
-    const addBtn = document.querySelector('.add__item-btn');
-    const cancelBtn = document.querySelector('.cancel__item-btn');
-    const textarea = document.querySelector('.textarea');
-    const form = document.querySelector('.form');
-    const edit = document.querySelector('.fas')
+function addTask(i) {
+  const btn = document.querySelectorAll(".add__btn");
+  const addBtn = document.querySelectorAll(".add__item-btn");
+  const cancelBtn = document.querySelectorAll(".cancel__item-btn");
+  const textarea = document.querySelectorAll(".textarea");
+  const form = document.querySelectorAll(".form");
 
-    let value
+    form[i].style.display = "block";
+    btn[i].style.display = "none";
+    addBtn[i].style.display = "none";
+    textarea[i].focus();
+    textarea[i].addEventListener("input", (e) => {
+      value = e.target.value;
+      if (value) {
+        addBtn[i].style.display = "block";
+      } else {
+        addBtn[i].style.display = "none";
+      }
+    });
+  
+  let value;
 
-    btn.addEventListener('click', () => {
-        form.style.display = 'block'
-        btn.style.display = 'none'
-        addBtn.style.display = 'none'
+  const addItem = function (value) {
+    const newItem = document.createElement("div");
+    newItem.classList.add("list__item");
+    newItem.setAttribute("contenteditable", true);
+    newItem.draggable = true;
+    newItem.textContent = value;
+    lists[i].append(newItem);
+    textarea[i].value = "";
+    value = "";
+    form[i].style.display = "none";
+    btn[i].style.display = "flex";
+    dragNdrop();
+  };
 
-        textarea.addEventListener('input', e => {
-            value = e.target.value
+  cancelBtn[i].addEventListener("click", () => {
+    textarea[i].value = "";
+    value = "";
+    form[i].style.display = "none";
+    btn[i].style.display = "flex";
+  });
 
-            if(value) {
-                addBtn.style.display = 'block'
-            } else {
-                addBtn.style.display = 'none'
-            }
-        })
-    })
+  addBtn[i].addEventListener("click", () => {
+    addItem(value);
+  });
 
-    cancelBtn.addEventListener('click', () => {
-        textarea.value = ''
-        value = ''
-        form.style.display = 'none'
-        btn.style.display = 'flex'
-    })
-
-    addBtn.addEventListener('click', () => {
-        const newItem = document.createElement('div')
-        newItem.classList.add('list__item')
-        newItem.setAttribute('contenteditable', true)
-        newItem.draggable = true
-        newItem.textContent = value
-        lists[0].append(newItem)
-
-        textarea.value = ''
-        value = ''
-        form.style.display = 'none'
-        btn.style.display = 'flex'
-
-        dragNdrop()
-    })
+  form[i].addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+      addItem(value);
+    }
+  });
 }
-addTask();
 
 function addBoard() {
-    const boards = document.querySelector('.boards')
-    const board = document.createElement('div')
-    board.classList.add('boards__item')
-    board.innerHTML = `<span contenteditable="true" class="title">Backlog</span>
-    <div class="list"></div>`
-    
-    boards.append(board)
-
-    changeTitle()
-    dragNdrop()
+  const boards = document.querySelector(".boards");
+  const board = document.createElement("div");
+  board.classList.add("boards__item");
+  boardIdNo += 1;
+  board.setAttribute("id", `no${boardIdNo}`);
+  board.innerHTML = `<span contenteditable="true" class="title">Backlog ${boardIdNo}</span>
+  <div class="list">
+</div>
+  <div class="form">
+  <textarea
+    class="textarea"
+    placeholder="Name your card..."
+  ></textarea>
+  <div class="buttons">
+    <button class="add__item-btn">Add card</button>
+    <button class="cancel__item-btn">Cancel</button>
+  </div>
+</div>
+  <div class="add__btn">
+  <span class="add__btn-plus"> + </span> Add card
+</div>`;
+  boards.append(board);
+  dragNdrop();
 }
-button.addEventListener('click', addBoard);
-
-function changeTitle() {
-    const titles = document.querySelectorAll('.title')
-
-    titles.forEach(title => {
-        title.addEventListener('click', e => e.target.textContent = '')
-    })
-}
-changeTitle()
+button.addEventListener("click", addBoard);
 
 let draggedItem = null;
 
-function dragNdrop() {
-    const listItems = document.querySelectorAll('.list__item')
-    const lists = document.querySelectorAll('.list')
+const dragNdrop = function () {
+  const listItems = document.querySelectorAll(".list__item");
+  const lists = document.querySelectorAll(".list");
+  // const boardItemEdit = document.querySelector('.board__row__item--edit'); /*TODO: remove opacity when dragging*
 
-    for (let i = 0; i < listItems.length; i++) {
-        const item = listItems[i]
+  listItems.forEach(function (item) {
+    item.addEventListener("dragstart", function () {
+      draggedItem = item;
+      item.classList.add("dragging");
+    });
+    item.addEventListener("dragend", function () {
+      item.classList.remove("dragging");
+    });
+    item.addEventListener("dblclick", () => {
+      item.remove();
+    });
+  });
 
-        item.addEventListener('dragstart', () => {
-            draggedItem = item
-            setTimeout(() => {
-                item.style.display = 'none'
-            }, 0)
-        })
+  lists.forEach(function (container) {
+    container.addEventListener("dragover", function (e) {
+      e.preventDefault();
+      const item = document.querySelector(".dragging");
+      const afterElement = getDragAfterElement(container, e.clientY);
 
-        item.addEventListener('dragend', () => {
-            setTimeout(() => {
-                item.style.display = 'block'
-                draggedItem = null
-            }, 0)
-        })
+      if (afterElement == null) {
+        container.appendChild(item);
+      } else {
+        container.insertBefore(item, afterElement);
+      }
+    });
 
-        item.addEventListener('dblclick', () => {
-            item.remove()
-        })
+    container.addEventListener("dragenter", function (e) {
+      e.preventDefault();
+      this.style.backgroundColor = "rgba(0,0,0,.3)";
+    });
 
-        for (let j = 0; j < lists.length; j++) {
-            const list = lists[j]
+    container.addEventListener("dragleave", function (e) {
+      this.style.backgroundColor = "rgba(0,0,0, 0)";
+    });
 
-            list.addEventListener('dragover', e => e.preventDefault())
-            list.addEventListener('dragenter', function(e) {
-                e.preventDefault()
-                this.style.backgroundColor = 'rgba(0,0,0,.3)'
-            })
+    container.addEventListener("drop", function (e) {
+      this.style.backgroundColor = "rgba(0,0,0, 0)";
+    });
+  });
+};
 
-            list.addEventListener('dragleave', function(e) {
-                this.style.backgroundColor = 'rgba(0,0,0, 0)'
-            })
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll(".list__item:not(.dragging)"),
+  ];
 
-            list.addEventListener('drop', function(e) {
-                this.style.backgroundColor = 'rgba(0,0,0, 0)'
-                this.append(draggedItem)
-            })
-        }
-    }
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
 }
 
-dragNdrop()
+dragNdrop();
